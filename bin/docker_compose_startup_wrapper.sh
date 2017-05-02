fc823d1a482dcedf0f2f095274246fb42f8d2cd0
pocari <caffelattenonsugar@gmail.com>
2017-05-02 15:46:09 +0900

    yarn installを追加

--- bin/docker_compose_startup_wrapper.sh
+++ bin/docker_compose_startup_wrapper.sh
@@ -8,5 +8,8 @@ export LOCAL_DOCKER_COMPOSE_MODE=1
 echo start bundle install
 bundle install --path=vendor/bundle
 
+echo start yarn install
+bin/rails webpacker:yarn_install
+
 bin/start_server.sh
 
